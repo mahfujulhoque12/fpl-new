8b3b87617c5a77da7887407dfdfb7b220d079c03
mahfujulhoque12 <mdmahfujulhoque123@gmail.com>
2025-04-16 10:12:57 +0600

    add-moreimg-portfolio

--- src/components/portfolio/PortfolioDetails.tsx
+++ src/components/portfolio/PortfolioDetails.tsx
@@ -1,6 +1,5 @@
 "use client";
 import MaxWidthWrapper from "@/components/layout/MaxWidthWrapper";
-
 import SubHeading from "@/components/typography/SubHeading";
 import { images } from "@/data/portfolioData";
 import Image from "next/image";
@@ -14,67 +13,77 @@ type ParamsProps = {
 
 const PortfolioDetails: React.FC<ParamsProps> = ({ params }) => {
   const datas = images.find((img) => img.slug === params.slug);
+
+  if (!datas || !datas.imgs) {
+    return (
+      <MaxWidthWrapper>
+        <p className="text-center my-10">
+          No images found for this portfolio item.
+        </p>
+      </MaxWidthWrapper>
+    );
+  }
+
   return (
     <MaxWidthWrapper>
-      <div className="">
-        <SubHeading className="font-bold text-center uppercase my-10">
-          Project Details
-        </SubHeading>
-        {/* Row 1: Images 1 and 2 */}
-        <div className="flex flex-col sm:flex-row  gap-5 mt-10">
-          {datas?.imgs.slice(0, 2).map((img, index) => (
-            <Image
-              key={index}
-              src={img}
-              width={500}
-              height={400}
-              alt={`img-${index}`}
-              className="rounded-md shadow-md w-full max-h-[400px] border object-cover"
-            />
-          ))}
-        </div>
+      <SubHeading className="font-bold text-center uppercase my-10">
+        Project Details
+      </SubHeading>
 
-        {/* Row 2: Image 3 */}
-        <div className="w-full">
-          {datas?.imgs.slice(2, 3).map((img, index) => (
-            <Image
-              key={index + 2}
-              src={img}
-              width={500}
-              height={400}
-              alt={`img-${index + 2}`}
-              className="my-8 rounded-md shadow-md w-full  max-h-[600px] border object-cover"
-            />
-          ))}
-        </div>
+      {/* Row 1: Images 1 and 2 */}
+      <div className="flex flex-col sm:flex-row gap-5 mt-10">
+        {datas.imgs.slice(0, 2).map((img, index) => (
+          <Image
+            key={index}
+            src={img}
+            width={500}
+            height={400}
+            alt={`img-${index}`}
+            className="rounded-md shadow-md w-full max-h-[400px] border object-cover"
+          />
+        ))}
+      </div>
 
-        {/* Row 3: Image 4 */}
-        <div className="w-full">
-          {datas?.imgs.slice(3, 4).map((img, index) => (
-            <Image
-              key={index + 3}
-              src={img}
-              width={500}
-              height={400}
-              alt={`img-${index + 3}`}
-              className="rounded-md shadow-md w-full  max-h-[600px] border object-cover"
-            />
-          ))}
-        </div>
+      {/* Row 2: Image 3 */}
+      <div className="w-full">
+        {datas.imgs.slice(2, 3).map((img, index) => (
+          <Image
+            key={index + 2}
+            src={img}
+            width={500}
+            height={400}
+            alt={`img-${index + 2}`}
+            className="my-8 rounded-md shadow-md w-full max-h-[600px] border object-cover"
+          />
+        ))}
+      </div>
 
-        {/* Row 4: Images 5 and 6 */}
-        <div className="grid grid-cols-1 sm:grid-cols-2  gap-5 mt-5 mb-10">
-          {datas?.imgs.slice(4).map((img, index) => (
-            <Image
-              key={index + 4}
-              src={img}
-              width={500}
-              height={400}
-              alt={`img-${index + 4}`}
-              className=" rounded-md shadow-md w-full object-cover border max-h-[400px]"
-            />
-          ))}
-        </div>
+      {/* Row 3: Image 4 */}
+      <div className="w-full">
+        {datas.imgs.slice(3, 4).map((img, index) => (
+          <Image
+            key={index + 3}
+            src={img}
+            width={500}
+            height={400}
+            alt={`img-${index + 3}`}
+            className="rounded-md shadow-md w-full max-h-[600px] border object-cover"
+          />
+        ))}
+      </div>
+
+      {/* Row 4: Images 5 and 6 */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 mt-5 mb-10">
+        {datas.imgs.slice(4).map((img, index) => (
+          <Image
+            key={index + 4}
+            src={img}
+            width={500}
+            height={400}
+            alt={`img-${index + 4}`}
+            className="rounded-md shadow-md w-full object-cover border max-h-[400px]"
+          />
+        ))}
       </div>
     </MaxWidthWrapper>
   );
